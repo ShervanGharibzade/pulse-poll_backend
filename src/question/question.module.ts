9250b1d955b3a37134e93d0ff5648758f149e259
shervan <shervangh@gmail.com>
2025-01-13 22:32:28 +0330

    add TypeOrmModule imports for Question, Answer, and User entities

--- src/question/question.module.ts
+++ src/question/question.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { QuestionController } from './question.controller';
 import { QuestionService } from './question.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Question } from 'src/entities/question.entity';
+import { Answer } from 'src/entities/answer.entity';
+import { User } from 'src/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Question, Answer, User])],
   controllers: [QuestionController],
   providers: [QuestionService],
 })
