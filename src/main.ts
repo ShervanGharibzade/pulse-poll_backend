4a7c2a1936c983b4cbcdae8aa016e297a3e19d9e
Shervan Gharibzadeh <104317474+ShervanGharibzade@users.noreply.github.com>
2025-01-20 09:21:41 +0330

    set origin

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 8080);
+  app.enableCors({
+    origin:['*'],
+    methods: ["GET", "POST"],
+    credentials: true,
 }
 bootstrap();
